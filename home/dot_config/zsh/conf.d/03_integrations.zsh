c5c3e29f908ab6e5313677c923bafb23f4c1728d
Yihuang Yu <yuyihuang@live.com>
2026-04-15 10:41:56 +0800

    chore: update and remove redhat related configurations
    - drop rio
    - update some config
    - remove most redhat related configurations
    
    Signed-off-by: Yihuang Yu <yuyihuang@live.com>

--- home/dot_config/zsh/conf.d/03_integrations.zsh
+++ home/dot_config/zsh/conf.d/03_integrations.zsh
@@ -6,8 +6,8 @@ autoload -Uz compinit
 zstyle ':completion:*' cache-path "${XDG_CACHE_HOME}/zsh/zcompcache"
 compinit -Cd "${XDG_CACHE_HOME}/zsh/zcompdump"
 
-(( $+commands[atuin] )) && eval "$(atuin init zsh --disable-up-arrow)"
-(( $+commands[direnv] )) && eval "$(direnv hook zsh)"
-(( $+commands[fzf] )) && source <(fzf --zsh)
-(( $+commands[mise] )) && eval "$(mise activate zsh)"
-(( $+commands[zoxide] )) && eval "$(zoxide init zsh --cmd j)"
+(($+commands[atuin])) && eval "$(atuin init zsh --disable-up-arrow)"
+(($+commands[direnv])) && eval "$(direnv hook zsh)"
+(($+commands[fzf])) && source <(fzf --zsh)
+(($+commands[mise])) && eval "$(mise activate zsh)"
+(($+commands[zoxide])) && eval "$(zoxide init zsh --cmd j)"
